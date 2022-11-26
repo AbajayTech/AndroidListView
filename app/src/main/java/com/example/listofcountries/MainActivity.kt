121bf5edc16454a31df29753cac9455a904ff48b
RayAbajila <rayabajila@gmail.com>
2022-11-26 15:52:14 +0300

    initial commit

---
+++ app/src/main/java/com/example/listofcountries/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.listofcountries
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listview=findViewById<ListView>(R.id.lstview)
+        //val listView:ListView=findViewById(R.id.lstview)
+
+        val majina= arrayOf("Kenya","Tanzania","Uganda","Ethiopia","Rwanda","Burundi")
+        val arrayAdapter:ArrayAdapter<String> =
+            ArrayAdapter(this,android.R.layout.simple_list_item_1,majina)
+        listview.adapter=arrayAdapter
+
+        listview.setOnItemClickListener { adapterView, view, position, l ->
+            Toast.makeText(this,"You have clicked on " +majina[position],Toast.LENGTH_LONG).show()
+        }
+    }
+}
